24263a843ddfe88e32f9db9c583c342e58c4f6cf
Bryan de Bourbon <bryandebourbon@gmail.com>
2024-02-27 22:19:25 -0500

    Initial Commit

---
+++ snapclone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  snapclone
+//
+//  Created by Bryan de Bourbon on 2/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ snapclone/snapcloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  snapcloneApp.swift
+//  snapclone
+//
+//  Created by Bryan de Bourbon on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct snapcloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
